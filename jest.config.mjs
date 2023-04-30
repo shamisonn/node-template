ebf6befe23189d25c881aaad3902575edd63569e
shamisonn <unknown0incident@gmail.com>
2023-04-30 15:01:47 +0900

    add: test by jest

---
+++ jest.config.mjs
@@ -0,0 +1,21 @@
+/** @type {import('@jest/types').Config.InitialOptions} */
+const config = {
+  moduleFileExtensions: ["ts", "js"],
+  roots: ["<rootDir>/src"],
+  testMatch: [
+    "**/__tests__/**/*.+(ts|tsx|js)",
+    "**/?(*.)+(spec|test).+(ts|tsx|js)",
+  ],
+  transform: {
+    "^.+\\.ts$": [
+      "ts-jest",
+      {
+        tsconfig: "tsconfig.json",
+      },
+    ],
+  },
+  moduleNameMapper: {
+    "^@/(.*)$": "<rootDir>/src/$1",
+  },
+};
+export default config;
---
+++ src/hoge.test.ts
@@ -0,0 +1,7 @@
+import { hoge } from "@/hoge";
+
+describe("hoge", () => {
+  it("equal fuga", () => {
+    expect(hoge).toBe("fuga");
+  });
+});
